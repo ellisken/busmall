0a0a442568a7da4f25de7312b0c9c77b646fc94c
ellisken <ellisken@oregonstate.edu>
2018-08-13 15:19:09 -0700

    added event listener to images for voting

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ var productCount = 20; //Should be set to equal the initial number of products
 //Product info to load
 var productInfo = [['r2d2 bag', 'img/bag.jpg'], ['banana slicer', 'img/banana.jpg'],
   ['ipad toilet paper holder', 'img/bathroom.jpg'], ['breakfast bot', 'img/breakfast.jpg'],
-  ['useless boots', 'img/boots.jpg'], ['meatball gum', 'img/bubblgum.jpg'], 
+  ['useless boots', 'img/boots.jpg'], ['meatball gum', 'img/bubblegum.jpg'], 
   ['scared chair', 'img/chair.jpg'], ['cthulhu', 'img/cthulhu.jpg'],
-  ['duck snout', 'img/dog-duck.jpg'], ['dragon meat', 'img/dragonmeat.jpg'],
+  ['duck snout', 'img/dog-duck.jpg'], ['dragon meat', 'img/dragon.jpg'],
   ['utensil pen', 'img/pen.jpg'], ['mop slippers', 'img/pet-sweep.jpg'],
   ['pizza shears', 'img/scissors.jpg'], ['shark sleeping bag', 'img/shark.jpg'],
   ['baby mop', 'img/sweep.png'], ['tauntaun bag', 'img/tauntaun.jpg'],
@@ -33,7 +33,7 @@ function Product(name, filepath, description){
 
 Product.listOfProducts = []; //Stores all Product objects created
 Product.totalVotes = 0; //Tracks total votes made, reset to zero on page refresh
-Product.currentProductsDisplayed = [-1, -1, -1]; //Tracks which products are currently displayed
+Product.currentProductsDisplayed = []; //Tracks which products are currently displayed
 
 
 
@@ -41,38 +41,47 @@ Product.currentProductsDisplayed = [-1, -1, -1]; //Tracks which products are cur
  *                 Function Definitions 
  ******************************************************/
 
-//Generates three random numbers, checking that each number
-//is unique and is not in Product.currentProductsDisplayed.
-//Then replaces the contents of Product.currentProductsDisplayed
-//with those numbers, and adds the corresponding products in 
-//Product.listOfProducts to the page for voting
+//Displays three new products to the page
 function displayThreeNewProducts(){
   //Generate 3 unique random numbers
   // in range of 0 - productCount
   var newProducts = [];
   var randIndex;
-  console.log('starting random number gen');
   for(var i=0; i < 3; i++){
     do{
       randIndex = Math.floor(Math.random() * productCount);
-      console.log('random index = ' + randIndex);
     }while(newProducts.includes(randIndex) || Product.currentProductsDisplayed.includes(randIndex));
     //add random number to randomNumbers
     newProducts.push(randIndex);
   }
-
-  console.log(`newProducts has indices: ${newProducts}`);
 
   //Display each of the products and update respective display counts
   for(var j=0; j < 3; j++){
     var product = document.getElementsByTagName('img')[j];
     var newProductIndex = newProducts[j];
     product.src = Product.listOfProducts[newProductIndex].filepath;
+    product.alt = Product.listOfProducts[newProductIndex].name;//make alt == product name
+    product.id = newProductIndex; //Set id == the index of that product in Product list
     Product.listOfProducts[newProductIndex].displayCount++;
   }
+
+  //Replace contents of currentProductsDisplayed with new Products
+  Product.currentProductsDisplayed = newProducts;
   return;
 }
 
+
+//Displays the voting results in order from
+//most popular to least
+
+// Replace section header to specify results
+// Remove content from ul of voting images
+// Grab the ordered list element 
+// Sort the list of products by total vote count
+// For each image in the list of products
+// 	Create a list item, and an image item (append to appropriate parents)
+// 	Set the image src = to product source 
+// 	Set a label with “n votes for the product_name”
 function displayResults(){
 
 }
@@ -87,12 +96,32 @@ for(var i=0; i < productCount; i++){
   var j = 0;
   new Product(productInfo[i][j++], productInfo[i][j]);
 }
+
 console.log('created product objects');
 
-//On page load/refresh, display 3 pictures
+//On page load, display 3 pictures
 displayThreeNewProducts();
+console.log('displayed 3 initial products');
+
+//Get image elements
+var productsOnDisplay = document.getElementsByTagName('img');
+
+//Add event listener to each image
+for(var j=0; j < 3; j++){
+  var selectedProduct = productsOnDisplay[j];
+  selectedProduct.addEventListener('click', function(e){
+    var currentProductIndex = e.target.id; //get index of clicked product
+    Product.listOfProducts[currentProductIndex].voteCount++; //increment vote count for clicked product
+    
+    //If vote count == 25, display results!
+  
+  });
+}
 
 //On image click, display 3 new products (making sure to update
+
+
+
 //vote count). If voteCt == 25
 // Replace section header to specify results
 // Remove content from ul of voting images
